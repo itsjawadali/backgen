c019c46de80229226c392a76f70ed50824613a3f
itsjawadali <jawwad865@gmail.com>
2022-11-28 15:46:02 +0500

    added new files

---
+++ test.js
@@ -0,0 +1,15 @@
+var h5 = document.querySelector("h5");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector(".gradient");
+
+function setgradient(){
+	body.style.background = "linear-gradient(to right, " +
+	color1.value + ", " + color2.value +
+	")";
+	h5.textContent = body.style.background + ";";
+}
+color1.addEventListener("input", setgradient);
+
+color2.addEventListener("input", setgradient);
+
